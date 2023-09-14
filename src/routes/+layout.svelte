d31572842370bebe651e97211fa0c757bf06f653
jill64 <intents.turrets0h@icloud.com>
2023-09-14 16:23:46 +0900

    add preview site

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import { ThemeManager } from '$lib'
+</script>
+
+<ThemeManager />
+<h1>svelte-dark-theme</h1>
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import { setting, theme } from '$lib'
+</script>
+
+<div>Theme: {$theme}</div>
+<div>Setting: {$setting}</div>
+<label>
+  <input type="radio" bind:group={$setting} value="dark" />
+  Dark
+</label>
+<label>
+  <input type="radio" bind:group={$setting} value="light" />
+  Light
+</label>
+<label>
+  <input type="radio" bind:group={$setting} value="sync" />
+  Sync
+</label>
+
+<style>
+  :global(body) {
+    font-family: sans-serif;
+  }
+  :global(body.dark) {
+    background-color: #222;
+    color: #eee;
+  }
+</style>
