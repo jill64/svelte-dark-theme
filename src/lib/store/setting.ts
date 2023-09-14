1f8621692a34a2419e50aef16fb9cde1ffa39c84
jill64 <intents.turrets0h@icloud.com>
2023-09-14 17:12:38 +0900

    feat: sync theme between tabs

--- src/lib/store/setting.ts
+++ src/lib/store/setting.ts
@@ -1,13 +1,31 @@
+import { browser } from '$app/environment'
+import { attempt } from '@jill64/attempt'
 import { typedStorage } from '@jill64/typed-storage'
 import { writable } from 'svelte/store'
 import type { ThemeSetting } from '../types/ThemeSetting'
 import { isThemeSetting } from '../util/isThemeSetting'
 
-const storage = typedStorage('svelte-dark-theme', {
+const storageKey = 'svelte-dark-theme'
+
+const storage = typedStorage(storageKey, {
   guard: isThemeSetting
 })
 
 const { subscribe, set } = writable<ThemeSetting>(storage.get() ?? 'sync')
+
+if (browser) {
+  addEventListener('storage', ({ key, newValue }) => {
+    if (key !== storageKey || !newValue) {
+      return
+    }
+
+    const value = attempt(() => JSON.parse(newValue), null)
+
+    if (isThemeSetting(value)) {
+      set(value)
+    }
+  })
+}
 
 export const setting = {
   subscribe,
