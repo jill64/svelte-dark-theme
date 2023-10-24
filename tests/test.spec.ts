1ea1a48bffd6db053b76e3afe1152bd77b5ee2d0
jill64 <intents.turrets0h@icloud.com>
2023-10-24 23:38:06 +0900

    update test config

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,12 +1,95 @@
 import { expect, test } from '@playwright/test'
 
-test.use({
-  colorScheme: 'dark'
-})
+test('light-mode', async ({ page }) => {
+  await page.emulateMedia({ colorScheme: 'light' })
 
-test('test', async ({ page }) => {
   await page.goto('/svelte-dark-theme')
   await expect(
     page.getByRole('heading', { name: 'svelte-dark-theme' })
   ).toBeVisible()
+
+  await expect(page.getByText('Theme: light')).toBeVisible()
+  await expect(page.getByText('Theme: dark')).not.toBeVisible()
+  await expect(page.getByText('Setting: light')).not.toBeVisible()
+  await expect(page.getByText('Setting: dark')).not.toBeVisible()
+  await expect(page.getByText('Setting: sync')).toBeVisible()
+
+  await page.getByLabel('Dark').click()
+
+  await expect(page.getByText('Theme: light')).not.toBeVisible()
+  await expect(page.getByText('Theme: dark')).toBeVisible()
+  await expect(page.getByText('Setting: light')).not.toBeVisible()
+  await expect(page.getByText('Setting: dark')).toBeVisible()
+  await expect(page.getByText('Setting: sync')).not.toBeVisible()
+
+  await page.reload()
+
+  await expect(page.getByText('Theme: light')).not.toBeVisible()
+  await expect(page.getByText('Theme: dark')).toBeVisible()
+  await expect(page.getByText('Setting: light')).not.toBeVisible()
+  await expect(page.getByText('Setting: dark')).toBeVisible()
+  await expect(page.getByText('Setting: sync')).not.toBeVisible()
+
+  await page.getByLabel('Light').click()
+
+  await expect(page.getByText('Theme: light')).toBeVisible()
+  await expect(page.getByText('Theme: dark')).not.toBeVisible()
+  await expect(page.getByText('Setting: light')).toBeVisible()
+  await expect(page.getByText('Setting: dark')).not.toBeVisible()
+  await expect(page.getByText('Setting: sync')).not.toBeVisible()
+
+  await page.reload()
+
+  await expect(page.getByText('Theme: light')).toBeVisible()
+  await expect(page.getByText('Theme: Dark')).not.toBeVisible()
+  await expect(page.getByText('Setting: light')).toBeVisible()
+  await expect(page.getByText('Setting: dark')).not.toBeVisible()
+  await expect(page.getByText('Setting: sync')).not.toBeVisible()
+
+  await page.getByLabel('Sync').click()
+
+  await expect(page.getByText('Theme: light')).toBeVisible()
+  await expect(page.getByText('Theme: Dark')).not.toBeVisible()
+  await expect(page.getByText('Setting: light')).not.toBeVisible()
+  await expect(page.getByText('Setting: dark')).not.toBeVisible()
+  await expect(page.getByText('Setting: sync')).toBeVisible()
+})
+
+test('dark-mode', async ({ page }) => {
+  await page.emulateMedia({ colorScheme: 'dark' })
+
+  await page.goto('/svelte-dark-theme')
+  await expect(
+    page.getByRole('heading', { name: 'svelte-dark-theme' })
+  ).toBeVisible()
+
+  await expect(page.getByText('Theme: light')).not.toBeVisible()
+  await expect(page.getByText('Theme: dark')).toBeVisible()
+  await expect(page.getByText('Setting: light')).not.toBeVisible()
+  await expect(page.getByText('Setting: dark')).not.toBeVisible()
+  await expect(page.getByText('Setting: sync')).toBeVisible()
+
+  await page.getByLabel('Light').click()
+
+  await expect(page.getByText('Theme: light')).toBeVisible()
+  await expect(page.getByText('Theme: dark')).not.toBeVisible()
+  await expect(page.getByText('Setting: light')).toBeVisible()
+  await expect(page.getByText('Setting: dark')).not.toBeVisible()
+  await expect(page.getByText('Setting: sync')).not.toBeVisible()
+
+  await page.getByLabel('Dark').click()
+
+  await expect(page.getByText('Theme: light')).not.toBeVisible()
+  await expect(page.getByText('Theme: Dark')).toBeVisible()
+  await expect(page.getByText('Setting: light')).not.toBeVisible()
+  await expect(page.getByText('Setting: dark')).toBeVisible()
+  await expect(page.getByText('Setting: sync')).not.toBeVisible()
+
+  await page.getByLabel('Sync').click()
+
+  await expect(page.getByText('Theme: light')).not.toBeVisible()
+  await expect(page.getByText('Theme: Dark')).toBeVisible()
+  await expect(page.getByText('Setting: light')).not.toBeVisible()
+  await expect(page.getByText('Setting: dark')).not.toBeVisible()
+  await expect(page.getByText('Setting: sync')).toBeVisible()
 })
