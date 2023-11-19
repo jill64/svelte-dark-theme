213ab03f9a2584ba16de5d698261e8b65f2b6eb8
jill64 <intents.turrets0h@icloud.com>
2023-11-19 21:05:02 +0900

    embed sentry dsn

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,6 +1,7 @@
-import { PUBLIC_SENTRY_DSN } from '$env/static/public'
 import { clientInit } from '@jill64/sentry-sveltekit-cloudflare'
 
-const onError = clientInit(PUBLIC_SENTRY_DSN)
+const onError = clientInit(
+  'https://93b1fe4d87dc3ee2c537dcf653407c17@o4505814639312896.ingest.sentry.io/4506161182539776'
+)
 
 export const handleError = onError()
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,9 @@
 import { onRender } from '$lib'
 import { serverInit } from '@jill64/sentry-sveltekit-cloudflare'
-import { PUBLIC_SENTRY_DSN } from '$env/static/public'
 
-const { onHandle, onError } = serverInit(PUBLIC_SENTRY_DSN)
+const { onHandle, onError } = serverInit(
+  'https://93b1fe4d87dc3ee2c537dcf653407c17@o4505814639312896.ingest.sentry.io/4506161182539776'
+)
 
 export const handle = onHandle(onRender())
 export const handleError = onError()
