dab866f8a8b6a8c51acd6be4d205ec7674cb72a8
jill64 <intents.turrets0h@icloud.com>
2023-12-11 04:52:04 +0900

    chore: refactor demo with npm-demo-layout

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,6 +1,6 @@
-import { clientInit } from '@jill64/sentry-sveltekit-cloudflare'
+import { init } from '@jill64/sentry-sveltekit-cloudflare/client'
 
-const onError = clientInit(
+const onError = init(
   'https://93b1fe4d87dc3ee2c537dcf653407c17@o4505814639312896.ingest.sentry.io/4506161182539776'
 )
 
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { Layout } from '@jill64/npm-demo-layout'
+  import packageJson from '../../package.json'
+</script>
+
+<Layout {packageJson}>
+  <slot />
+</Layout>
