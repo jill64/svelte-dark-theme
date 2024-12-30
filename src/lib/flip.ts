65dc699731fc8dc17ee23a1150bd370a3506afff
jill64 <intents.turrets0h@icloud.com>
2024-12-30 21:33:27 +0900

    refactor: consolidate theme management into a single store and update related components

--- src/lib/flip.ts
+++ src/lib/flip.ts
@@ -1,14 +1,8 @@
-import { isDark } from '@jill64/svelte-device-theme'
-import { derived, type Readable } from 'svelte/store'
-import { setting } from './store/setting'
-import { theme } from './store/theme'
+import { is } from '@jill64/svelte-device-theme'
+import { store } from './store.svelte'
 
-export const flip: Readable<() => void> = derived(
-  [theme, isDark],
-  ([$theme, $isDark]) =>
-    () => {
-      const next = $theme === 'dark' ? 'light' : 'dark'
-      const config = (next === 'dark') === $isDark ? 'sync' : next
-      setting.set(config)
-    }
-)
+export const flip = $derived(() => {
+  const next = store.theme === 'dark' ? 'light' : 'dark'
+  const config = (next === 'dark') === is.dark ? 'sync' : next
+  store.setting = config
+})
---
+++ src/lib/store.svelte.ts
@@ -0,0 +1,47 @@
+import { browser } from '$app/environment'
+import { themeSetting } from '$lib/enum/themeSetting'
+import { is } from '@jill64/svelte-device-theme'
+import { storage } from '@jill64/svelte-storage'
+import { enums } from '@jill64/svelte-storage/serde'
+import type { ThemeSetting } from './types/ThemeSetting'
+import type { ThemeValue } from './types/ThemeValue'
+import { isThemeValue } from './util/isThemeValue'
+
+let server = $state<ThemeValue>('light')
+
+let local = storage({ 'svelte-dark-theme': enums(themeSetting, 'sync') })
+
+let localTheme = $state<ThemeSetting>('sync')
+
+localTheme = $derived(local['svelte-dark-theme'])
+
+let setting = $state<ThemeSetting>('sync')
+
+let theme = $derived(
+  browser
+    ? isThemeValue(setting)
+      ? setting
+      : is.dark
+        ? 'dark'
+        : 'light'
+    : server
+)
+
+export const store = {
+  get server() {
+    return server
+  },
+  set server(value) {
+    server = value
+  },
+  get setting() {
+    return setting
+  },
+  set setting(value) {
+    setting = value
+    local['svelte-dark-theme'] = value
+  },
+  get theme() {
+    return theme
+  }
+}
