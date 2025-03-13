ca07d45ea0d8d9a9710e2c07c79e13d05ca170ea
jill64 <contact@jill64.dev>
2025-03-14 07:48:25 +0900

    fix: update eslint configuration to ignore specific file patterns

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,6 @@
 import { svelteTsConfig } from '@jill64/eslint-config-svelte'
 
 /** @type {import('@jill64/eslint-config-svelte').FlatConfig[]} */
-export default svelteTsConfig()
+export default svelteTsConfig({
+  ignores: ['**/*.svelte.ts', '.svelte-kit']
+})
